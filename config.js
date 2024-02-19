7e1f366378520b6e22c53d24c0fbaba0f9da755f
lamia <lamianisha3@gmai..com>
2024-02-19 23:56:54 +0600

    second

--- config.js
+++ config.js
@@ -53,6 +53,28 @@ document.getElementById('a1').addEventListener('click', function () {
 
 })
 
+// coupon disable
+
+document.getElementById('delete-confirm').addEventListener('keyup', function (event) {
+
+  const text = event.target.value;
+  const deleteButton = document.getElementById('btn-delete');
+  if (text === 'NEW15' || text === 'COUPLE20') {
+    deleteButton.removeAttribute('disabled');
+  }
+  else {
+    deleteButton.setAttribute('disabled', true);
+  }
+
+})
+
+document.getElementById('btn-delete').addEventListener('click', function () {
+  const secret = document.getElementById('secret-info');
+  secret.style.display = 'none';
+})
+
+
+
 document.getElementById('a2').addEventListener('click', function () {
   a2.style.backgroundColor = '#1DD100';
 
@@ -233,8 +255,8 @@ document.getElementById('a4').addEventListener('click', function () {
 
 
 
-document.getElementById('a5').addEventListener('click', function () {
-  a5.style.backgroundColor = '#1DD100';
+document.getElementById('b1').addEventListener('click', function () {
+  b1.style.backgroundColor = '#1DD100';
 
   const kisu = document.getElementById('b1');
   const kisu2 = kisu.innerText;
@@ -293,10 +315,10 @@ document.getElementById('a5').addEventListener('click', function () {
 
 
 
-document.getElementById('a2').addEventListener('click', function () {
-  a2.style.backgroundColor = '#1DD100';
+document.getElementById('b2').addEventListener('click', function () {
+  b2.style.backgroundColor = '#1DD100';
 
-  const kisu = document.getElementById('a2');
+  const kisu = document.getElementById('b2');
   const kisu2 = kisu.innerText;
 
   const placeList = document.getElementById('section-box');
@@ -354,10 +376,10 @@ document.getElementById('a2').addEventListener('click', function () {
 
 
 
-document.getElementById('a2').addEventListener('click', function () {
-  a2.style.backgroundColor = '#1DD100';
+document.getElementById('b3').addEventListener('click', function () {
+  b3.style.backgroundColor = '#1DD100';
 
-  const kisu = document.getElementById('a2');
+  const kisu = document.getElementById('b3');
   const kisu2 = kisu.innerText;
 
   const placeList = document.getElementById('section-box');
@@ -415,73 +437,10 @@ document.getElementById('a2').addEventListener('click', function () {
 
 
 
-document.getElementById('a2').addEventListener('click', function () {
-  a2.style.backgroundColor = '#1DD100';
+document.getElementById('b4').addEventListener('click', function () {
+  b4.style.backgroundColor = '#1DD100';
 
-  const kisu = document.getElementById('a2');
-  const kisu2 = kisu.innerText;
-
-  const placeList = document.getElementById('section-box');
-
-  const p = document.createElement('p');
-  p.innerText = kisu2;
-
-  const h1 = document.createElement('h1');
-  h1.innerText = 'Economy';
-
-  const h2 = document.createElement('h2');
-  h2.innerText = '550';
-
-  placeList.appendChild(p);
-  placeList.appendChild(h1);
-  placeList.appendChild(h2);
-
-  // seatCounting
-  const add = document.getElementById('seat-counting');
-  const have = add.innerText;
-  const upd = parseInt(have);
-
-  const updatedScore = upd + 1;
-  add.innerText = updatedScore;
-
-  // seatNumberDecrease
-
-  const deduct = document.getElementById('seat-left');
-  const had = deduct.innerText;
-  const upda = parseInt(had);
-
-  const updatingScore = upda - 1;
-  deduct.innerText = updatingScore;
-
-
-  // totalPrice and grand price
-  const totalTicket = document.getElementById('seat-counting')
-  const totalPrice = document.getElementById('total-price')
-  const grandPrice = document.getElementById('grand-price')
-
-  const ticketText = totalTicket.innerText;
-  const setNumb = parseInt(ticketText);
-
-  const setPrice = setNumb * 550;
-  totalPrice.innerText = setPrice;
-  grandPrice.innerText = setPrice;
-
-})
-
-
-
-
-
-
-
-
-
-
-
-document.getElementById('a2').addEventListener('click', function () {
-  a2.style.backgroundColor = '#1DD100';
-
-  const kisu = document.getElementById('a2');
+  const kisu = document.getElementById('b4');
   const kisu2 = kisu.innerText;
 
   const placeList = document.getElementById('section-box');
@@ -541,134 +500,10 @@ document.getElementById('a2').addEventListener('click', function () {
 
 
 
+document.getElementById('c1').addEventListener('click', function () {
+  c1.style.backgroundColor = '#1DD100';
 
-document.getElementById('a2').addEventListener('click', function () {
-  a2.style.backgroundColor = '#1DD100';
-
-  const kisu = document.getElementById('a2');
-  const kisu2 = kisu.innerText;
-
-  const placeList = document.getElementById('section-box');
-
-  const p = document.createElement('p');
-  p.innerText = kisu2;
-
-  const h1 = document.createElement('h1');
-  h1.innerText = 'Economy';
-
-  const h2 = document.createElement('h2');
-  h2.innerText = '550';
-
-  placeList.appendChild(p);
-  placeList.appendChild(h1);
-  placeList.appendChild(h2);
-
-  // seatCounting
-  const add = document.getElementById('seat-counting');
-  const have = add.innerText;
-  const upd = parseInt(have);
-
-  const updatedScore = upd + 1;
-  add.innerText = updatedScore;
-
-  // seatNumberDecrease
-
-  const deduct = document.getElementById('seat-left');
-  const had = deduct.innerText;
-  const upda = parseInt(had);
-
-  const updatingScore = upda - 1;
-  deduct.innerText = updatingScore;
-
-
-  // totalPrice and grand price
-  const totalTicket = document.getElementById('seat-counting')
-  const totalPrice = document.getElementById('total-price')
-  const grandPrice = document.getElementById('grand-price')
-
-  const ticketText = totalTicket.innerText;
-  const setNumb = parseInt(ticketText);
-
-  const setPrice = setNumb * 550;
-  totalPrice.innerText = setPrice;
-  grandPrice.innerText = setPrice;
-
-})
-
-
-
-
-
-
-
-
-
-
-document.getElementById('a2').addEventListener('click', function () {
-  a2.style.backgroundColor = '#1DD100';
-
-  const kisu = document.getElementById('a2');
-  const kisu2 = kisu.innerText;
-
-  const placeList = document.getElementById('section-box');
-
-  const p = document.createElement('p');
-  p.innerText = kisu2;
-
-  const h1 = document.createElement('h1');
-  h1.innerText = 'Economy';
-
-  const h2 = document.createElement('h2');
-  h2.innerText = '550';
-
-  placeList.appendChild(p);
-  placeList.appendChild(h1);
-  placeList.appendChild(h2);
-
-  // seatCounting
-  const add = document.getElementById('seat-counting');
-  const have = add.innerText;
-  const upd = parseInt(have);
-
-  const updatedScore = upd + 1;
-  add.innerText = updatedScore;
-
-  // seatNumberDecrease
-
-  const deduct = document.getElementById('seat-left');
-  const had = deduct.innerText;
-  const upda = parseInt(had);
-
-  const updatingScore = upda - 1;
-  deduct.innerText = updatingScore;
-
-
-  // totalPrice and grand price
-  const totalTicket = document.getElementById('seat-counting')
-  const totalPrice = document.getElementById('total-price')
-  const grandPrice = document.getElementById('grand-price')
-
-  const ticketText = totalTicket.innerText;
-  const setNumb = parseInt(ticketText);
-
-  const setPrice = setNumb * 550;
-  totalPrice.innerText = setPrice;
-  grandPrice.innerText = setPrice;
-
-})
-
-
-
-
-
-
-
-
-
-document.getElementById('a2').addEventListener('click', function () {
-  a2.style.backgroundColor = '#1DD100';
-
-  const kisu = document.getElementById('a2');
+  const kisu = document.getElementById('c1');
   const kisu2 = kisu.innerText;
 
   const placeList = document.getElementById('section-box');
@@ -729,11 +564,10 @@ document.getElementById('a2').addEventListener('click', function () {
 
 
 
+document.getElementById('c2').addEventListener('click', function () {
+  c2.style.backgroundColor = '#1DD100';
 
-document.getElementById('a2').addEventListener('click', function () {
-  a2.style.backgroundColor = '#1DD100';
-
-  const kisu = document.getElementById('a2');
+  const kisu = document.getElementById('c2');
   const kisu2 = kisu.innerText;
 
   const placeList = document.getElementById('section-box');
@@ -792,10 +626,71 @@ document.getElementById('a2').addEventListener('click', function () {
 
 
 
-document.getElementById('a2').addEventListener('click', function () {
-  a2.style.backgroundColor = '#1DD100';
+document.getElementById('c3').addEventListener('click', function () {
+  c3.style.backgroundColor = '#1DD100';
 
-  const kisu = document.getElementById('a2');
+  const kisu = document.getElementById('c3');
+  const kisu2 = kisu.innerText;
+
+  const placeList = document.getElementById('section-box');
+
+  const p = document.createElement('p');
+  p.innerText = kisu2;
+
+  const h1 = document.createElement('h1');
+  h1.innerText = 'Economy';
+
+  const h2 = document.createElement('h2');
+  h2.innerText = '550';
+
+  placeList.appendChild(p);
+  placeList.appendChild(h1);
+  placeList.appendChild(h2);
+
+  // seatCounting
+  const add = document.getElementById('seat-counting');
+  const have = add.innerText;
+  const upd = parseInt(have);
+
+  const updatedScore = upd + 1;
+  add.innerText = updatedScore;
+
+  // seatNumberDecrease
+
+  const deduct = document.getElementById('seat-left');
+  const had = deduct.innerText;
+  const upda = parseInt(had);
+
+  const updatingScore = upda - 1;
+  deduct.innerText = updatingScore;
+
+
+  // totalPrice and grand price
+  const totalTicket = document.getElementById('seat-counting')
+  const totalPrice = document.getElementById('total-price')
+  const grandPrice = document.getElementById('grand-price')
+
+  const ticketText = totalTicket.innerText;
+  const setNumb = parseInt(ticketText);
+
+  const setPrice = setNumb * 550;
+  totalPrice.innerText = setPrice;
+  grandPrice.innerText = setPrice;
+
+})
+
+
+
+
+
+
+
+
+
+document.getElementById('c4').addEventListener('click', function () {
+  c4.style.backgroundColor = '#1DD100';
+
+  const kisu = document.getElementById('c4');
   const kisu2 = kisu.innerText;
 
   const placeList = document.getElementById('section-box');
@@ -854,10 +749,13 @@ document.getElementById('a2').addEventListener('click', function () {
 
 
 
-document.getElementById('a2').addEventListener('click', function () {
-  a2.style.backgroundColor = '#1DD100';
 
-  const kisu = document.getElementById('a2');
+
+
+document.getElementById('d1').addEventListener('click', function () {
+  d1.style.backgroundColor = '#1DD100';
+
+  const kisu = document.getElementById('d1');
   const kisu2 = kisu.innerText;
 
   const placeList = document.getElementById('section-box');
@@ -915,10 +813,11 @@ document.getElementById('a2').addEventListener('click', function () {
 
 
 
-document.getElementById('a2').addEventListener('click', function () {
-  a2.style.backgroundColor = '#1DD100';
 
-  const kisu = document.getElementById('a2');
+document.getElementById('d2').addEventListener('click', function () {
+  d2.style.backgroundColor = '#1DD100';
+
+  const kisu = document.getElementById('d2');
   const kisu2 = kisu.innerText;
 
   const placeList = document.getElementById('section-box');
@@ -974,29 +873,128 @@ document.getElementById('a2').addEventListener('click', function () {
 
 
 
-// grand price minus on coupon code
 
 
 
-// coupon disable
+document.getElementById('d3').addEventListener('click', function () {
+  d3.style.backgroundColor = '#1DD100';
 
-document.getElementById('delete-confirm').addEventListener('keyup', function (event) {
+  const kisu = document.getElementById('d3');
+  const kisu2 = kisu.innerText;
 
-const text=event.target.value;
-const deleteButton=document.getElementById('btn-delete');
-if (text === 'NEW15' || text ==='COUPLE20'){
-  deleteButton.removeAttribute('disabled');
-}
-else{
-  deleteButton.setAttribute('disabled',true);
-}
+  const placeList = document.getElementById('section-box');
+
+  const p = document.createElement('p');
+  p.innerText = kisu2;
+
+  const h1 = document.createElement('h1');
+  h1.innerText = 'Economy';
+
+  const h2 = document.createElement('h2');
+  h2.innerText = '550';
+
+  placeList.appendChild(p);
+  placeList.appendChild(h1);
+  placeList.appendChild(h2);
+
+  // seatCounting
+  const add = document.getElementById('seat-counting');
+  const have = add.innerText;
+  const upd = parseInt(have);
+
+  const updatedScore = upd + 1;
+  add.innerText = updatedScore;
+
+  // seatNumberDecrease
+
+  const deduct = document.getElementById('seat-left');
+  const had = deduct.innerText;
+  const upda = parseInt(had);
+
+  const updatingScore = upda - 1;
+  deduct.innerText = updatingScore;
+
+
+  // totalPrice and grand price
+  const totalTicket = document.getElementById('seat-counting')
+  const totalPrice = document.getElementById('total-price')
+  const grandPrice = document.getElementById('grand-price')
+
+  const ticketText = totalTicket.innerText;
+  const setNumb = parseInt(ticketText);
+
+  const setPrice = setNumb * 550;
+  totalPrice.innerText = setPrice;
+  grandPrice.innerText = setPrice;
 
 })
 
-document.getElementById('btn-delete').addEventListener('click',function(){
-  const secret =document.getElementById('secret-info');
-  secret.style.display='none';
+
+
+
+
+
+
+
+
+document.getElementById('d4').addEventListener('click', function () {
+  d4.style.backgroundColor = '#1DD100';
+
+  const kisu = document.getElementById('d4');
+  const kisu2 = kisu.innerText;
+
+  const placeList = document.getElementById('section-box');
+
+  const p = document.createElement('p');
+  p.innerText = kisu2;
+
+  const h1 = document.createElement('h1');
+  h1.innerText = 'Economy';
+
+  const h2 = document.createElement('h2');
+  h2.innerText = '550';
+
+  placeList.appendChild(p);
+  placeList.appendChild(h1);
+  placeList.appendChild(h2);
+
+  // seatCounting
+  const add = document.getElementById('seat-counting');
+  const have = add.innerText;
+  const upd = parseInt(have);
+
+  const updatedScore = upd + 1;
+  add.innerText = updatedScore;
+
+  // seatNumberDecrease
+
+  const deduct = document.getElementById('seat-left');
+  const had = deduct.innerText;
+  const upda = parseInt(had);
+
+  const updatingScore = upda - 1;
+  deduct.innerText = updatingScore;
+
+
+  // totalPrice and grand price
+  const totalTicket = document.getElementById('seat-counting')
+  const totalPrice = document.getElementById('total-price')
+  const grandPrice = document.getElementById('grand-price')
+
+  const ticketText = totalTicket.innerText;
+  const setNumb = parseInt(ticketText);
+
+  const setPrice = setNumb * 550;
+  totalPrice.innerText = setPrice;
+  grandPrice.innerText = setPrice;
+
 })
+
+
+
+
+
+
 
 
 
